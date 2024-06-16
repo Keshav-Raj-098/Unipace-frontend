ef3c326b6a6b130455aa2ee47e0deae82e2730bc
Keshav Raj <146741309+Keshav-Raj-098@users.noreply.github.com>
2024-06-17 02:39:16 +0530

    Update responsiveAppBar.js

--- src/components/responsiveAppBar.js
+++ src/components/responsiveAppBar.js
@@ -1,23 +1,27 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import { Container, Box} from '@mui/material';
-import Logo from '../assets/NAV-UNIPACE.png';
+import { Container, Box, Button, TextField} from '@mui/material';
+import Logo from '../assets/Asset 4.svg';
+import stlogo from "../assets/student.svg"
+import busslogo from "../assets/business.svg"
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+
 
 export default function ResponsiveAppBar({ mode, setMode }) {
   const navigate = useNavigate();
 
 
   return (
-    <AppBar position="fixed">
+    <AppBar position="fixed" >
       <Container>
         <Toolbar disableGutters>
           <Box
             sx={{
               height: '100%',
               display: 'flex',
-              justifyContent: 'start',
+              justifyContent: 'space-between',
               alignItems: 'center',
               flexGrow: 1
             }}
@@ -26,12 +30,50 @@ export default function ResponsiveAppBar({ mode, setMode }) {
               src={Logo}
               alt="Logo"
               loading="lazy"
+             
               width={150}
-              style={{ cursor: 'pointer' }}
+              height={40}
+              style={{ cursor: 'pointer',marginTop:"28px",transform: "scale(1.5)" }}
               onClick={() => {
                 navigate('/');
               }}
             />
+            <Box
+            
+            sx={{
+              padding:"5px",
+              display:"flex",
+              gap:"15px"
+              // backgroundColor:"red"
+            }}
+            >
+           
+
+          <button 
+           
+           style={{padding:"5px 10px", borderRadius:"15px ",border:"none",fontWeight:"bold", cursor:"pointer"
+
+            ,backgroundColor:"whitesmoke",color:"#1976d2"
+           }}
+           
+          onClick={() => {
+                navigate('/signin',{ state: { user: 'Student'}})}} >Student</button>
+          <button 
+           
+           style={{padding:"5px 10px", borderRadius:"15px ",border:"none",fontWeight:"bold", cursor:"pointer"
+
+            ,backgroundColor:"whitesmoke",color:"#1976d2"
+           }}
+           
+          onClick={() => {
+                navigate('/signin', { state: { user: 'Startup'}})}} >StartUp</button>
+
+
+
+
+            </Box>
+
+        
           </Box>
         </Toolbar>
       </Container>
